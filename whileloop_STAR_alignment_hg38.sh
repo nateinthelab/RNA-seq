9c952bbd64e65ca4e7d130e2d5108572c363b845
nateinthelab <148483144+nateinthelab@users.noreply.github.com>
2024-07-31 10:18:23 +0200

    Add files via upload

---
+++ whileloop_STAR_alignment_hg38.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define variables   
+THREADS=18                          # Number of threads to use
+
+# Get a list of _1.fq.gz files
+FILE_LIST=(*_1.fq.gz)
+INDEX=0
+NUM_FILES=${#FILE_LIST[@]}
+
+# While loop to process each file pair
+while [ $INDEX -lt $NUM_FILES ]; do
+  R1=${FILE_LIST[$INDEX]}
+  BASE=$(basename ${R1} _1.fq.gz)
+  R2="${BASE}_2.fq.gz"
+  
+  if [ -f "${R2}" ]; then
+    # Define the output prefix for this pair
+    OUTPUT_PREFIX="${BASE}."
+
+    # Print debug information
+    echo "Processing ${R1} and ${R2}"
+
+    # Run STAR
+    STAR --runThreadN ${THREADS} \
+         --genomeDir star_ref_hg38_Refseq \
+         --readFilesIn ${R1},${R2} \
+         --readFilesCommand zcat \
+         --outFileNamePrefix ${OUTPUT_PREFIX} \
+         --outSAMtype BAM Unsorted \
+         --sjdbGTFfile hg38.ncbiRefSeq.gtf \
+         --outFilterMultimapNmax 20 \
+         --alignSJoverhangMin 8 \
+         --alignSJDBoverhangMin 1 \
+         --outFilterMismatchNmax 999 \
+         --outFilterMismatchNoverLmax 0.04 \
+         --alignIntronMin 20 \
+         --alignIntronMax 1000000 \
+         --alignMatesGapMax 1000000
+  else
+    echo "Error: Corresponding file for ${R1} not found."
+  fi
+
+  # Increment the index
+  INDEX=$((INDEX + 1))
+done
